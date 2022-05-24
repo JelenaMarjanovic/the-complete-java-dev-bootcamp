f6358d4cef426c043b0c7881d8b26b0dea7b0175
jelenamarjanovic <jelena.marjanovic.13@gmail.com>
2022-05-24 13:13:14 +0200

    Challenge 1 - solution

--- Module 1 - Java Fundamentals/section_02/challenge 01/YourInitials.java
+++ Module 1 - Java Fundamentals/section_02/challenge 01/YourInitials.java
@@ -1,6 +1,12 @@
 public class YourInitials {
   public static void main(String[] args) {
-      //print initials here.
-      
+      // print initials here.
+      System.out.println("JJJJJJJ   M     M");
+      System.out.println("J     J   MM   MM");
+      System.out.println("      J   M M M M");
+      System.out.println("      J   M  M  M");
+      System.out.println("      J   M     M");
+      System.out.println("J     J   M     M");
+      System.out.println(" JJJJJ    M     M");
   }
 }
