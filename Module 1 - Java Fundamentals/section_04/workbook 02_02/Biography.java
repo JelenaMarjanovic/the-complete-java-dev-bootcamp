0fc0ab02da6551e4cabda755e50623b85ce15816
jelenamarjanovic <jelena.marjanovic.13@gmail.com>
2022-05-25 02:30:04 +0200

    Workbook 2.2 - solution

--- Module 1 - Java Fundamentals/section_04/workbook 02_02/Biography.java
+++ Module 1 - Java Fundamentals/section_04/workbook 02_02/Biography.java
@@ -1,19 +1,34 @@
 public class Biography {
   public static void main(String[] args) {
 
-      //make a name variable
-      //make an age variable
-      //make a country variable
-      //make a sport variable
-      //make an hours variable
-      //make a game variable
-      //make a subject variable
-      //make a grade variable
+      // make a name variable
+      String name = "Jelena";
+
+      // make an age variable
+      int age = 37;
+
+      // make a country variable
+      String country = "Serbia";
+
+      // make a sport variable
+      String sport = "basketball";
+
+      // make an hours variable
+      int hours = 3;
+
+      // make a game variable
+      String game = "chess";
+
+      // make a subject variable
+      String subject = "mathematics";
+
+      // make a grade variable
+      char grade = 'A';
    
-      System.out.println("My name is <name>. I'm <age> years old, and I'm from <country> ");
-      System.out.println("My favourite sport is <sport>. I play for <hours> hours a day");
-      System.out.println("When I'm tired, I like to play <game>.");
-      System.out.println("In school, my favourite subject was <subject>, I scored a <grade>.");
+      System.out.println("My name is " + name + ". I'm " + age + " years old, and I'm from " + country + ".");
+      System.out.println("My favourite sport is " + sport + ". I play for " + hours + " hours a day.");
+      System.out.println("When I'm tired, I like to play " + game + ".");
+      System.out.println("In school, my favourite subject was " + subject +", I scored an " + grade + ".");
 
   }
 }
