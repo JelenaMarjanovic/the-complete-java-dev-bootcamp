56fd041c4bc9909c1c0c34e24e2e6556317446e3
jelenamarjanovic <jelena.marjanovic.13@gmail.com>
2022-05-25 16:13:53 +0200

    Type-casting

---
+++ Module 1 - Java Fundamentals/section_04/TypeConversion.java
@@ -0,0 +1,9 @@
+public class TypeConversion {
+  public static void main(String[] args) {
+    double salary = 5833.333333 ;
+    System.out.println("Percy's monthly salary is $" + salary);
+
+    int roundedSalary = (int)salary;
+    System.out.println("Percy's monthly salary (rounded) is $" + roundedSalary);
+  }
+}
