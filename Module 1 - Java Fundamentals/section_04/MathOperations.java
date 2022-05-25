5061e79f8614e19d78aa96a30ae26ff0aa8bd084
jelenamarjanovic <jelena.marjanovic.13@gmail.com>
2022-05-25 13:52:22 +0200

    Math Operators - Part 2

--- Module 1 - Java Fundamentals/section_04/MathOperations.java
+++ Module 1 - Java Fundamentals/section_04/MathOperations.java
@@ -34,5 +34,27 @@ public class MathOperations {
     System.out.println("Originally, Fred had " + (bagOfSweets2 / bagOfSweets1) + " times more sweets than George.");
     System.out.println("Andromeda has " + (starsInAndromeda / starsInMilkyWay) + " times more stars.");
     System.out.println("Snape caught the error. Furious, he divided Ron's score to: " + (testScore / bonusMarks) + ".");
+
+    System.out.println();
+
+    // The % operator
+    System.out.println("10 is an even number, since dividing 10 by 2 has a remainder of: " + (10 % 2));
+    System.out.println("5 is an odd number, since dividing 5 by 2 has a remainder of: " + (5 % 2));
+
+    System.out.println();
+
+    int counter = 0;
+
+    // The ++ operator
+    counter++;
+    counter++;
+    counter++;
+    System.out.println("I count: " + counter);
+
+    // The -- operator
+    counter--;
+    counter--;
+    counter--;
+    System.out.println("I count: " + counter);
   }
 }
