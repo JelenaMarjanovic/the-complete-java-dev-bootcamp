cce38a5bacdc501765c005d26757f8127966ab30
jelenamarjanovic <jelena.marjanovic.13@gmail.com>
2022-05-25 02:14:11 +0200

    Type: String

--- Module 1 - Java Fundamentals/section_04/Bus.java
+++ Module 1 - Java Fundamentals/section_04/Bus.java
@@ -1,17 +1,24 @@
 public class Bus {
   public static void main(String[] args) {
+    String sentence = "Number of passengers: ";
+
     int passengers = 0;
+    System.out.println(sentence + passengers);
 
-    System.out.println("First stop");;
+    System.out.println("First stop...");;
     passengers += 9;
-    System.out.println(passengers);
+    System.out.println(sentence + passengers);
 
-    System.out.println("Dropping off");
+    System.out.println("Dropping off...");
     passengers -= 5;
-    System.out.println(passengers);
+    System.out.println(sentence + passengers);
 
-    System.out.println("Last stop");
+    System.out.println("Last stop...");
     passengers -= 4;
-    System.out.println(passengers);
+    System.out.println(sentence + passengers);
+
+    // sentence = "End of shift.";
+    // System.out.println(sentence);
+    System.out.println("End of shift.");
   }
 }
