c65721c6a1a30ec0ecf70fe58010255d0c2cec1b
jelenamarjanovic <jelena.marjanovic.13@gmail.com>
2022-05-26 02:46:55 +0200

    Scanner - Part 2

--- Module 1 - Java Fundamentals/section_04/Survey.java
+++ Module 1 - Java Fundamentals/section_04/Survey.java
@@ -3,24 +3,40 @@ import java.util.Scanner;
 public class Survey {
   public static void main(String[] args) {
     Scanner scan = new Scanner(System.in);
+    int counter = 0;
 
     System.out.println("Welcome! Thank you for taking the survey.");
     
-    System.out.println("What is your name?");
+    // Question 1
+    System.out.println("\nWhat is your name?");
+    counter++;
     String name = scan.nextLine();
 
-    System.out.println("How much money do you spend on coffee?");
+    // Question 2
+    System.out.println("\nHow much money do you spend on coffee?");
+    counter++;
     double coffeePrice = scan.nextDouble();
 
-    System.out.println("How much money do you spend on fast food?");
+    // Question 3
+    System.out.println("\nHow much money do you spend on fast food?");
+    counter++;
     double foodPrice = scan.nextDouble();
 
-    System.out.println("How many times a week do you buy coffee?");
+    // Question 4
+    System.out.println("\nHow many times a week do you buy coffee?");
+    counter++;
     int coffeeAmount = scan.nextInt();
 
-    System.out.println("How many time a week do you buy fast food?");
+    // Question 5
+    System.out.println("\nHow many time a week do you buy fast food?");
+    counter++;
     int foodAmount = scan.nextInt();
 
     scan.close();
+
+    System.out.println("\nThank you, " + name + ", for answering all " + counter + " questions.");
+    System.out.println("Your fast food expenses are " + (foodPrice / coffeePrice) + " times more than your coffee expenses.");
+    System.out.println("Weekly, you spend $" + (coffeeAmount * coffeePrice) + " on coffee.");
+    System.out.println("Weekly, you spend $" + (foodAmount * foodPrice) + " on food.");
   }
 }
