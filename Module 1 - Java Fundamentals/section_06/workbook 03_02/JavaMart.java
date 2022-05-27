c523c2e35a245c4aa8a36446b4c7dec7989f301a
jelenamarjanovic <jelena.marjanovic.13@gmail.com>
2022-05-27 13:41:16 +0200

    Workbook 3.2 - solution

--- Module 1 - Java Fundamentals/section_06/workbook 03_02/JavaMart.java
+++ Module 1 - Java Fundamentals/section_06/workbook 03_02/JavaMart.java
@@ -5,19 +5,22 @@ public class JavaMart {
       double toyCar = 5.99;
       System.out.println("Can I get this car?");
      
-      // if you have enough money
-          //       print: Sure!
-          //       pay for the toy car 
-
-      // else:  Sorry, I only have <wallet> left.
-
+      if (wallet >= toyCar) {
+        System.out.println("Sure!");
+        wallet -= toyCar;
+      } else {
+        System.out.println("Sorry, I only have " + wallet + " left.");
+      }
       
-      double nike = 95.99;       //run test case with 89.99
+      double nike = 95.99;
+      // double nike = 89.99;       // run test case with 89.99
       System.out.println("Can I get these nike shoes?");
-      // if you have enough money
-          //       print: Sure!
-          //       pay for the nike shoes 
 
-      // else: Sorry, I only have <wallet> left.
+      if (nike <= wallet) {
+          System.out.println("Sure!");
+          wallet -= nike;
+      } else {
+        System.out.println("Sorry, I only have " + wallet + " left.");
+      }
   }
 }
