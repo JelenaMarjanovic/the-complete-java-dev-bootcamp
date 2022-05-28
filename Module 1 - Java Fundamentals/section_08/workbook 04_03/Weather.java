3ce997200c289e3fbde04e5eddbe0ad15a314578
jelenamarjanovic <jelena.marjanovic.13@gmail.com>
2022-05-29 01:19:22 +0200

    Workbook 4.3 - solution

--- Module 1 - Java Fundamentals/section_08/workbook 04_03/Weather.java
+++ Module 1 - Java Fundamentals/section_08/workbook 04_03/Weather.java
@@ -1,35 +1,36 @@
 public class Weather {
   public static void main(String[] args) {
-      double noon = 77;          // temperature at noon in fahrenheit.
+      double noon = 77;          // temperature at noon in fahrenheit
       double evening = 61;       // temperature during the evening in fahrenheit
       double midnight = 55;      // temperature at midnight in fahrenheit
 
-      // Task 3 - Call the printTemperatures function. 
-      
-      
+      System.out.println("Temperature at noon");
+      printTemperatures(noon);
+
+      System.out.println("\nTemperature during the evening");
+      printTemperatures(evening);
+
+      System.out.println("\nTemperature at midnight");      
+      printTemperatures(midnight);
   }
-
-
-  // Task 1: Make a function here. See the doc comments below. 
 
   /**
    * Function name: fahrenheitToCelsius - converts fahrenheit to celsius
    * @param fahrenheit (double)
    * @return celsius (double)
-   * 
-   * Inside the function:
-   * 1. returns the temperature in celsius. C = (F - 32) * 5/9. 
    */
+  public static double fahrenheitToCelsius(double fahrenheit) {
+    double celsius = (fahrenheit - 32) * 5/9;
 
-   
-  // Task 2: Make a function here. See the doc comments below. 
-
+    return celsius;
+  }
+  
   /**
    * Function name: printTemperatures – prints both temperature values. 
    * @param fahrenheit (double)
-   * 
-   * Inside the function: 
-   *  1. prints: F: <temperature in fahrenheit>.
-   *  2. prints: C: <temperature in celsius> . 
    */
+  public static void printTemperatures(double fahrenheit) {
+    System.out.println("F: " + fahrenheit);
+    System.out.println("C: " + fahrenheitToCelsius(fahrenheit));
+  }
 }
