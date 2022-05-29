103cde8a079a5f3dcde87a9b52b3a1018d7fe0cf
jelenamarjanovic <jelena.marjanovic.13@gmail.com>
2022-05-29 15:59:11 +0200

    Workbook 5.2 - solution

--- Module 1 - Java Fundamentals/section_10/workbook 05_02/Detention.java
+++ Module 1 - Java Fundamentals/section_10/workbook 05_02/Detention.java
@@ -1,8 +1,19 @@
+import java.util.Scanner;
+
 public class Detention {
   public static void main(String[] args) {
 
-      System.out.println("Hi Bart. I can write lines for you.\nWhat do you want me to write?");
-      // Task 1 – Scan for the nextLine(), and print it 99 times. 
+    Scanner scan = new Scanner(System.in);
+
+    System.out.println("Hi Bart. I can write lines for you.\nWhat do you want me to write?");
+
+    String sentence = scan.nextLine();
+
+    for (int i = 0; i < 99; i++) {
+      System.out.println(sentence);
+    }
+
+    scan.close();
       
   }
 }
