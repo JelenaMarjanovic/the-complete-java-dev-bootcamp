5f7b232b8ec50465d802e7dd72c32934859303d0
jelenamarjanovic <jelena.marjanovic.13@gmail.com>
2022-05-31 22:07:37 +0200

    Reference Trap

---
+++ Module 1 - Java Fundamentals/section_12/ReferenceTrap.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+
+public class ReferenceTrap {
+  public static void main(String[] args) {
+    String[] staffLastYear = {"Tommy", "Joel", "Ellie"};
+    // String[] staffThisYear = staffLastYear;
+
+    // String[] staffThisYear = copyStaff(staffLastYear);
+
+    String[] staffThisYear = Arrays.copyOf(staffLastYear, staffLastYear.length);
+
+    staffThisYear[1] = "Abby";
+
+    System.out.println("Staff last year:\n" + Arrays.toString(staffLastYear));
+    System.out.println("\nStaff this year:\n" + Arrays.toString(staffThisYear));
+  }
+
+  /**
+   * Function name - copyStaff
+   * @param staff (String[])
+   * @return (String[])
+   * 
+   * 1. Inside the function make a copy of provided array and return it.
+   */
+  public static String[] copyStaff(String[] staff) {
+    String[] staffCopy = new String[staff.length];
+
+    for (int i = 0; i < staffCopy.length; i++) {
+      staffCopy[i] = staff[i];
+    }
+
+    return staffCopy;
+  }
+}
