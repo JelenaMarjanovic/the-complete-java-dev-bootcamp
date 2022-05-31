38e1ae71c1240f8df9de6db3ac411cde968f7b90
jelenamarjanovic <jelena.marjanovic.13@gmail.com>
2022-05-31 13:17:22 +0200

    Updating Arrays

---
+++ Module 1 - Java Fundamentals/section_12/UpdatingArrays.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+public class UpdatingArrays {
+  public static void main(String[] args) {
+    String[] menu = {"Espresso", "Iced Coffee", "Macchiato"};
+
+    System.out.println("Menu:");
+    System.out.println(Arrays.toString(menu));
+
+    menu[2] = "Latte";
+
+    System.out.println("\nMenu after update:");
+    System.out.println(Arrays.toString(menu));
+
+    String[] newMenu = new String[5];
+
+    for (int i = 0; i < menu.length; i++) {
+      newMenu[i] = menu[i];
+    }
+
+    newMenu[3] = "House Blend";
+    newMenu[4] = "Dark Roast";
+
+    System.out.println("\nNew menu:");
+    System.out.println(Arrays.toString(newMenu));
+  }
+}
