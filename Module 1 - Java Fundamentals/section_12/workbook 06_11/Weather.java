ed319bb4bda4b1be1e877d8303304ee9b4237c24
jelenamarjanovic <jelena.marjanovic.13@gmail.com>
2022-05-31 23:10:50 +0200

    Workbook 6.11 - solution

--- Module 1 - Java Fundamentals/section_12/workbook 06_11/Weather.java
+++ Module 1 - Java Fundamentals/section_12/workbook 06_11/Weather.java
@@ -1,18 +1,16 @@
-import java.util.Arrays;
-
 public class Weather {
     public static void main(String[] args) {
         
         double[] celsius = {12.5, 14.5, 17.0, 21.0, 23.0, 18.5, 20.0};
         double[] fahrenheit;
         
-        // Task 2, call celsiusToFahrenheit and store the result in the fahrenheit array. 
-
-        // Task 4, Call printTemperatures for celsius and fahrenheit.
-
+        fahrenheit = celsiusToFahrenheit(celsius);
+        
+        printTemperatures(celsius, "Celsius");
+        printTemperatures(fahrenheit, "Fahrenheit");
     }
 
-    /** Task 1
+    /**
      * Function name: celsiusToFahrenheit. 
      * @param celsius ( double[] )
      * @return fahrenheit ( double[] )
@@ -22,9 +20,18 @@ public class Weather {
      *    2. Copy all the values from celsius into the fahrenheit array.
      *    3. Update all the values in the array according to: F = (C/5 * 9) + 32.
      *    4. return the fahrenheit array.
-     */   
+     */
+    public static double[] celsiusToFahrenheit(double[] celsius) {
+      double[] fahrenheit = new double[celsius.length];
+      
+      for (int i = 0; i < fahrenheit.length; i++) {
+        fahrenheit[i] = (celsius[i] / 5 * 9) + 32;
+      }
 
-    /** Task 3
+      return fahrenheit;
+    }
+
+    /**
      * Function name - printTemperatures
      * @param temp ( double[] )
      * @param type ( String ) can be either Celsius or Fahrenheit
@@ -33,7 +40,18 @@ public class Weather {
      *   1. Uses a for loop to print the temperature values on the SAME line.
      *       Example:  Celsius: [Celsius values separated by a space]
      *   2. Adds a new line of space after printing the contents of the array.
-     *   3. (Task 5) Rounds every temperature value to two decimal places. 
+     *   3. Rounds every temperature value to two decimal places. 
      */
-    
+    public static void printTemperatures(double[] temp, String type) {
+      if (!(type.equals("Celsius") || type.equals("Fahrenheit"))) {
+        System.out.println("Wrong temperature type!");
+        System.exit(0);
+      }
+
+      System.out.print(type + ": ");
+      for (int i = 0; i < temp.length; i++) {
+        System.out.print(String.format("%.2f", temp[i]) + " ");
+      }
+      System.out.println();
+    }
 }
