88c5b3502efd162340871fdbe6e23cafa00f5dd3
jelenamarjanovic <jelena.marjanovic.13@gmail.com>
2022-06-01 01:23:20 +0200

    Looping 2D Arrays - Nested Loops

--- Module 1 - Java Fundamentals/section_12/TDArrays.java
+++ Module 1 - Java Fundamentals/section_12/TDArrays.java
@@ -8,6 +8,7 @@ public class TDArrays {
       {65, 64, 61, 67}, // Ron's grades
       {95, 98, 99, 100}  // Hermione's grades
     };
+    String[] names = {"Harry", "Ron", "Hermione"};
 
     // // Harry's grades
     // grades[0][0] = 72;
@@ -27,13 +28,32 @@ public class TDArrays {
     // grades[2][2] = 99;
     // grades[2][3] = 100;
 
-    System.out.println("Grades:");
-
+    // System.out.println("Grades:");
     // System.out.println("\tHarry:\t\t" + Arrays.toString(grades[0]));
     // System.out.println("\tRon:\t\t" + Arrays.toString(grades[1]));
     // System.out.println("\tHermione:\t" + Arrays.toString(grades[2]));
-    System.out.println("\tHarry:\t\t" + grades[0][0] + " " + grades[0][1] + " " + grades[0][2] + " " + grades[0][3]);
-    System.out.println("\tRon:\t\t" + grades[1][0] + " " + grades[1][1] + " " + grades[1][2] + " " + grades[1][3]);
-    System.out.println("\tHermione:\t" + grades[2][0] + " " + grades[2][1] + " " + grades[2][2] + " " + grades[2][3]);
+    // System.out.println("\tHarry:\t\t" + grades[0][0] + " " + grades[0][1] + " " + grades[0][2] + " " + grades[0][3]);
+    // System.out.println("\tRon:\t\t" + grades[1][0] + " " + grades[1][1] + " " + grades[1][2] + " " + grades[1][3]);
+    // System.out.println("\tHermione:\t" + grades[2][0] + " " + grades[2][1] + " " + grades[2][2] + " " + grades[2][3]);
+
+    printGrades(grades, names);
+  }
+
+  /**
+   * Function name - printGrades
+   * @param grades (int[][])
+   * @param names  (String[])
+   * 
+   * Inside the function, prints grades by name, line by line
+   */
+  public static void printGrades(int[][] grades, String[] names) {
+    System.out.println("Grades:");
+    for (int i = 0; i < grades.length; i++) {
+      System.out.print("\t" + names[i] + "'s grades: ");
+      for (int j = 0; j < grades[i].length; j++) {
+        System.out.print(grades[i][j] + " ");
+      }
+      System.out.println();
+    }
   }
 }
