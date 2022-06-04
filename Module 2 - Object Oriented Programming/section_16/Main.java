c664fdc581eaa88b975f4fd88ef158bf7e835b12
jelenamarjanovic <jelena.marjanovic.13@gmail.com>
2022-06-05 01:42:00 +0200

    Car object

---
+++ Module 2 - Object Oriented Programming/section_16/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+  public static void main(String[] args) {
+    Car nissan = new Car();
+
+    nissan.make = "Nissan";
+    nissan.price = 5000;
+    nissan.year = 2020;
+    nissan.color = "red";
+
+    // System.out.println(nissan.make);
+    // System.out.println(nissan.price);
+    // System.out.println(nissan.year);
+    // System.out.println(nissan.color);
+
+    Car dodge = new Car();
+
+    dodge.make = "Dodge";
+    dodge.price = 11000;
+    dodge.year = 2019;
+    dodge.color = "blue";
+
+    System.out.println("This " + nissan.make + " is worth $" + nissan.price + ". It was built in " + nissan.year
+      + ". It is " + nissan.color + ".\n");
+    System.out.println("This " + dodge.make + " is worth $" + dodge.price + ". It was built in " + dodge.year
+      + ". It is " + dodge.color + ".\n");
+  }
+}
