fea4a6aa256c83c4a0cf6b2fd58f05551a6ed289
jelenamarjanovic <jelena.marjanovic.13@gmail.com>
2022-06-05 05:34:05 +0200

    Big 3 - Step 1 - Constructor

--- Module 2 - Object Oriented Programming/section_16/Main.java
+++ Module 2 - Object Oriented Programming/section_16/Main.java
@@ -1,23 +1,23 @@
 public class Main {
   public static void main(String[] args) {
-    Car nissan = new Car();
+    Car nissan = new Car("Nissan", 5000, 2020, "red");
 
-    nissan.make = "Nissan";
-    nissan.price = 5000;
-    nissan.year = 2020;
-    nissan.color = "red";
+    // nissan.make = "Nissan";
+    // nissan.price = 5000;
+    // nissan.year = 2020;
+    // nissan.color = "red";
 
     // System.out.println(nissan.make);
     // System.out.println(nissan.price);
     // System.out.println(nissan.year);
     // System.out.println(nissan.color);
 
-    Car dodge = new Car();
+    Car dodge = new Car("Dodge", 11000, 2019, "blue");
 
-    dodge.make = "Dodge";
-    dodge.price = 11000;
-    dodge.year = 2019;
-    dodge.color = "blue";
+    // dodge.make = "Dodge";
+    // dodge.price = 11000;
+    // dodge.year = 2019;
+    // dodge.color = "blue";
 
     System.out.println("This " + nissan.make + " is worth $" + nissan.price + ". It was built in " + nissan.year
       + ". It is " + nissan.color + ".\n");
