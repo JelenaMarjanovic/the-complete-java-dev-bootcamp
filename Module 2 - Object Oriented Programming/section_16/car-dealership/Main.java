9e474014c185fc05766a91e2f20f0bc093d53a87
jelenamarjanovic <jelena.marjanovic.13@gmail.com>
2022-06-06 16:11:07 +0200

    Dealership - move to folder and clean up the code

---
+++ Module 2 - Object Oriented Programming/section_16/car-dealership/Main.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class Main {
+  public static void main(String[] args) {
+    Car[] cars = new Car[] {
+      new Car("Nissan", 5000, 2020, "red", new String[] {"tires", "keys"}),
+      new Car("Dodge", 8500, 2019, "blue", new String[] {"tires", "rim"}),
+      new Car("Nissan", 5000, 2020, "yellow", new String[] {"tires", "filter"})
+    };
+
+    Dealership dealership = new Dealership();
+
+    for (int i = 0; i < cars.length; i++) {
+      dealership.setCar(cars[i], i);
+    }
+
+    System.out.println(dealership.getCar(0));
+  }
+}
