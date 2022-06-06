cc7a527f02ee83c8d05ef3cfecee36817db02487
jelenamarjanovic <jelena.marjanovic.13@gmail.com>
2022-06-06 11:54:21 +0200

    Reference Trap

--- Module 2 - Object Oriented Programming/section_16/Main.java
+++ Module 2 - Object Oriented Programming/section_16/Main.java
@@ -1,6 +1,8 @@
 public class Main {
   public static void main(String[] args) {
     Car nissan = new Car("Nissan", 5000, 2020, "red");
+    // Car nissan2 = nissan; // Reference trap
+    Car nissan2 = new Car("Nissan", 5000, 2020, "red");
 
     // nissan.make = "Nissan";
     // nissan.price = 5000;
@@ -17,6 +19,8 @@ public class Main {
     nissan.setColor("Jet Black");
     dodge.setColor("Jet Black");
 
+    nissan2.setColor("yellow");
+
     nissan.setPrice(nissan.getPrice() / 2);
     dodge.setPrice(dodge.getPrice() / 2);
 
@@ -29,5 +33,7 @@ public class Main {
       + ". It is " + nissan.getColor() + ".\n");
     System.out.println("This " + dodge.getMake() + " is worth $" + dodge.getPrice() + ". It was built in " + dodge.getYear()
       + ". It is " + dodge.getColor() + ".\n");
+    System.out.println("This " + nissan2.getMake() + " is worth $" + nissan2.getPrice() + ". It was built in " + nissan2.getYear()
+      + ". It is " + nissan2.getColor() + ".\n");
   }
 }
