36d4531b1d31334d64560adbf5c160ac091fb31b
jelenamarjanovic <jelena.marjanovic.13@gmail.com>
2022-06-06 12:07:07 +0200

    Workbook - Part 4

--- Module 2 - Object Oriented Programming/section_16/airline-reservation-system/Main.java
+++ Module 2 - Object Oriented Programming/section_16/airline-reservation-system/Main.java
@@ -2,11 +2,17 @@ import java.util.Arrays;
 
 public class Main {
   public static void main(String[] args) {
-    Person p = new Person("Rayan Slim", "Canadian", "01/01/1111", 5);
+    Person p1 = new Person("Rayan Slim", "Canadian", "01/01/1111", 5);
+    Person p2 = new Person(p1);
 
-    p.setSeatNumber(10);
+    p1.setSeatNumber(10);
 
-    System.out.println("Name: " + p.getName() + "\nNationality: " + p.getNationality()
-      + "\nDate of Birth: " + p.getDateOfBirth() +"\nSeat Number: " + p.getSeatNumber() + "\n");
+    p2.setName("Jad Slim");
+    p2.setSeatNumber(3);
+
+    System.out.println("Name: " + p1.getName() + "\nNationality: " + p1.getNationality()
+      + "\nDate of Birth: " + p1.getDateOfBirth() +"\nSeat Number: " + p1.getSeatNumber() + "\n");
+    System.out.println("Name: " + p2.getName() + "\nNationality: " + p2.getNationality()
+      + "\nDate of Birth: " + p2.getDateOfBirth() +"\nSeat Number: " + p2.getSeatNumber() + "\n");
   }
 }
