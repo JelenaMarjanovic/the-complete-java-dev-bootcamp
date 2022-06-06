bfc7d18a2299f698b3e2e27d0bac3f7d585a5af1
jelenamarjanovic <jelena.marjanovic.13@gmail.com>
2022-06-07 01:47:11 +0200

    Fixing the Constructor - Dealership

--- Module 2 - Object Oriented Programming/section_16/car-dealership/Dealership.java
+++ Module 2 - Object Oriented Programming/section_16/car-dealership/Dealership.java
@@ -1,8 +1,12 @@
 public class Dealership {
   private Car[] cars;
 
-  public Dealership() {
-    this.cars = new Car[3];
+  public Dealership(Car[] cars) {
+    this.cars = new Car[cars.length];
+
+    for (int i = 0; i < cars.length; i++) {
+      this.cars[i] = new Car(cars[i]);
+    }
   }
 
   public Car getCar(int idx) {
--- Module 2 - Object Oriented Programming/section_16/car-dealership/Main.java
+++ Module 2 - Object Oriented Programming/section_16/car-dealership/Main.java
@@ -5,23 +5,13 @@ public class Main {
     Car[] cars = new Car[] {
       new Car("Nissan", 5000, 2020, "red", new String[] {"tires", "keys"}),
       new Car("Dodge", 8500, 2019, "blue", new String[] {"tires", "rim"}),
-      new Car("Nissan", 5000, 2017, "yellow", new String[] {"tires", "filter"})
+      new Car("Nissan", 5000, 2017, "yellow", new String[] {"tires", "filter"}),
+      new Car("Honda", 7000, 2019, "orange", new String[] {"tires, filter"}),
+      new Car("Mercedes", 12000, 2015, "jet black", new String[] {"tires", "filter", "transmission"})
     };
 
-    Dealership dealership = new Dealership();
+    Dealership dealership = new Dealership(cars);
 
-    for (int i = 0; i < cars.length; i++) {
-      dealership.setCar(cars[i], i);
-    }
-
-    dealership.sell(2);
-
-    System.out.println("Searching for Nissan with $3000...");
-    System.out.println(dealership.search("Nissan", 3000));
-
-    System.out.println("Searching for Dodge with $10000...");
-    System.out.println(dealership.search("Dodge", 10000));
-
-    System.out.println(dealership);
+    System.out.println(dealership);    
   }
 }
