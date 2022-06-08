9febb048f05ba7fc3cdaceaa7480428e41397519
jelenamarjanovic <jelena.marjanovic.13@gmail.com>
2022-06-08 09:40:28 +0200

    Vending Machine - Part 1

--- Module 2 - Object Oriented Programming/section_17/vending-machine/Main.java
+++ Module 2 - Object Oriented Programming/section_17/vending-machine/Main.java
@@ -11,6 +11,10 @@ public class Main {
       { new Item("Crush", 1.99, 2) , new Item("C-Cola", 1.49, 2), new Item("Berry", 2.49, 1) }
     };
     */
+
+    Item pepsi = new Item("Pepsi", 1.99, 3);
+
+    System.out.println(pepsi);
   
   }
 }
