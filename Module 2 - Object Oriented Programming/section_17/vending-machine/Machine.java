02faea6354e623efbd563ad0b7f530ed07f96b41
jelenamarjanovic <jelena.marjanovic.13@gmail.com>
2022-06-08 12:45:23 +0200

    Vending Machine - Part 2

--- Module 2 - Object Oriented Programming/section_17/vending-machine/Machine.java
+++ Module 2 - Object Oriented Programming/section_17/vending-machine/Machine.java
@@ -1,5 +1,23 @@
 public class Machine {
-  
+  private Item[][] items;
+
+  public Machine(Item[][] items) {
+    this.items = new Item[items.length][items[0].length];
+
+    for (int i = 0; i < items.length; i++) {
+      for (int j = 0; j < items[i].length; j++) {
+        this.items[i][j] = new Item(items[i][j]);
+      }
+    }
+  }
+
+  public Item getItem(int row, int spot) {
+    return new Item(this.items[row][spot]);
+  }
+
+  public void setItem(Item item, int row, int spot) {
+    this.items[row][spot] = new Item(item);
+  }
 
   /**
    * Function name – dispense
@@ -12,5 +30,15 @@ public class Machine {
    *      • if so: decreases its quantity by one and returns true.
    *      • otherwise: returns false.
    */
-  
+  public boolean dispense(int row, int spot) {
+    Item currentItem = this.items[row][spot];
+    int currentQuantity = currentItem.getQuantity();
+
+    if (currentQuantity > 0) {
+      currentItem.setQuantity(currentQuantity - 1);
+      return true;
+    }
+
+    return false;
+  }
 }
--- Module 2 - Object Oriented Programming/section_17/vending-machine/Main.java
+++ Module 2 - Object Oriented Programming/section_17/vending-machine/Main.java
@@ -4,17 +4,32 @@ public class Main {
     System.out.println("\t             WELCOME TO JAVA DRINKS!            ");
     System.out.println("\t************************************************");
     
-    /*
     Item[][] items = new Item[][] {
       { new Item("Pepsi", 1.99, 3) , new Item("Fresca", 1.49, 3), new Item("Brisk", 2.49, 2) },
       { new Item("Fanta", 1.99, 2) , new Item("Barq's", 1.49, 2), new Item("A & W", 2.49, 3) },
       { new Item("Crush", 1.99, 2) , new Item("C-Cola", 1.49, 2), new Item("Berry", 2.49, 1) }
     };
-    */
 
-    Item pepsi = new Item("Pepsi", 1.99, 3);
+    Machine machine = new Machine(items);
+    
+    // Test the constructor
+    // machine.getItem(2, 1).setPrice(2.99);
+    // System.out.println(machine.getItem(2, 1));
 
-    System.out.println(pepsi);
-  
+    // Test the getter
+    // Item item = machine.getItem(2, 1);
+    // item.setPrice(2.99);
+    // System.out.println(machine.getItem(2, 1));
+
+    // Test the setter
+    // Item item = machine.getItem(2, 1);
+    // item.setPrice(2.99);
+    // machine.setItem(item, 2, 1);
+    // System.out.println(machine.getItem(2, 1));
+
+    // Test the action
+    machine.dispense(0, 0);
+    machine.dispense(0, 0);
+    System.out.println(machine.getItem(0, 0));
   }
 }
