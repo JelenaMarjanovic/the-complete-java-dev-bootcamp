b671bd5566fe9e690463adaf34ff38e00dd989b6
jelenamarjanovic <jelena.marjanovic.13@gmail.com>
2022-06-10 13:25:08 +0200

    Debugging - Exercise 2 - add test cases

--- Module 2 - Object Oriented Programming/section_18/debugging/exercise_02/Conditionals.java
+++ Module 2 - Object Oriented Programming/section_18/debugging/exercise_02/Conditionals.java
@@ -1,11 +1,20 @@
 public class Conditionals {
   public static void main(String[] args) {
+    // Test Case 1
     int age = 24;
     double debt = 4000;
 
     // // Test Case 2
     // int age = 16;
     // double debt = 4000;
+
+    // // Test Case 3
+    // int age = 16;
+    // double debt = 0;
+
+    // // Test Case 4
+    // int age = 24;
+    // double debt = 0;
 
     // String creditScore = debt > 0 ? "Good" : "Bad";
     String creditScore = debt > 0 ? "Bad" : "Good";
