4d7a650b4dd1dabd4af89dec2c03c094b936d9df
jelenamarjanovic <jelena.marjanovic.13@gmail.com>
2022-06-17 00:00:55 +0200

    Debugging - Exercise 7

---
+++ Module 2 - Object Oriented Programming/section_18/debugging/exercise_07/Matrix.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+public class Matrix {
+  public static void main(String[] args) {
+    int[][] matrix = { // matrix means 2d array
+        { 0, 0, 0 },
+        { 0, 0, 0 },
+        { 0, 0, 0 }
+    };
+
+    // 1. for (int i = 0; i < matrix.length; i--) {
+    for (int i = 0; i < matrix.length; i++) {
+      // 2. for (int j = 0; j < matrix.length; i++) {
+      for (int j = 0; j < matrix.length; j++) {
+        // 3. matrix[i][j] = i;
+        matrix[j][i] = i;
+      }
+    }
+
+    printMatrix(matrix);
+  }
+
+  public static void printMatrix(int[][] matrix) {
+    for (int i = 0; i < matrix.length; i++) {
+      System.out.println(Arrays.toString(matrix[i]));
+    }
+  }
+}
